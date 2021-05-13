66848da3dd52fee55b79fbc940a8618a5c64e667
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-05-13 08:46:32 +0200

    try with conda install from upstream

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,7 @@ cd $CONSTRUCT_ROOT
 # Constructor should be latest for non-native building
 # See https://github.com/conda/constructor
 echo "***** Install constructor *****"
-conda install -y "constructor>=3.1.0" jinja2 curl libarchive -c conda-forge --override-channels
+conda install -y 'constructor>=3.1.0' jinja2 -c conda-forge -c defaults --override-channels
 if [[ "$(uname)" == "Darwin" ]]; then
     conda install -y coreutils -c conda-forge --override-channels
 elif [[ "$(uname)" == MINGW* ]]; then
