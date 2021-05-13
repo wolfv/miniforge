5a6cd188689c21466e52aaff803c06e46d74b9b9
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-05-13 10:31:44 +0200

    use defaults on ppc64le

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,11 +10,12 @@ cd $CONSTRUCT_ROOT
 # Constructor should be latest for non-native building
 # See https://github.com/conda/constructor
 echo "***** Install constructor *****"
-if [[ "$(TARGET_PLATFORM)" == "linux-ppc64le" ]]; then
-   conda install -y 'constructor>=3.1.0' jinja2 -c conda-forge -c defaults --override-channels
-else
-   conda install -y "constructor>=3.1.0" jinja2 curl libarchive -c conda-forge --override-channels
+if [[ "${TARGET_PLATFORM}" == "linux-ppc64le" ]]; then
+  USE_DEFAULTS="-c defaults"
 fi
+
+conda install -y "constructor>=3.1.0" jinja2 curl libarchive -c conda-forge $USE_DEFAULTS --override-channels
+
 
 if [[ "$(uname)" == "Darwin" ]]; then
     conda install -y coreutils -c conda-forge --override-channels
